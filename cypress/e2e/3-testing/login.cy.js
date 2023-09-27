f97a19eda66b16f9dadd33c369e4af508031a871
lindatarod85 <lindatarod@gmail.com>
2023-09-27 21:19:38 +0200

    x

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -5,7 +5,15 @@ it("signup and login user", () => {
   cy.get("#loginPassword").type("Test1234");
   cy.get("#loginForm .btn-success").click();
 
-  cy.location("pathname").should("eq", "/?view=profile&name=linda");
+  cy.location().should((location) => {
+    expect(location.href).to.eq(
+      "http://127.0.0.1:5500/?view=profile&name=linda",
+    );
+
+    // expect(location.pathname).to.eq('/commands/location')
+  });
+
+  //cy.location("pathname").should("eq", "/?view=profile&name=linda");
 
   //logout
   //cy.visit('http://127.0.0.1:5500/profile.html');
