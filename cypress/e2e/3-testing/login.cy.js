221667159725131bfbfaba7b883ef1bffc2a5dc7
lindatarod85 <lindatarod@gmail.com>
2023-09-27 16:53:36 +0200

    updates

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -2,8 +2,8 @@ it("signup and login user", () => {
   cy.visit("http://127.0.0.1:5500/index.html");
 
   cy.get('input[name="email"]').type("linda@stud.noroff.no");
-  //cy.get('input[name="password"]').type("Test1234");
-  cy.get(".modal-foter.btn-success").click();
+  cy.get('input[name="password"]').type("Test1234");
+  cy.get(".modal-foter .btn-success").click();
 
   cy.location("pathname").should("eq", "/profile.html");
 
