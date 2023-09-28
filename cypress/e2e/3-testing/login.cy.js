38dd2f92718a0954d2f43d20acb523d2495b2faf
lindatarod85 <lindatarod@gmail.com>
2023-09-28 09:18:46 +0200

    x

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,7 +1,12 @@
 it("signup and login user", () => {
   cy.visit("http://127.0.0.1:5500/index.html");
 
-  cy.get("#registerForm .btn-outline-success").click();
+  cy.get("#registerName").type("Linda");
+  cy.get("#registerEmail").type("linda@stud.noroff.no");
+  cy.get("#registerPassword").type("Test1234");
+  cy.get("#registerForm .btn-success").click();
+
+  //cy.get("#registerForm .btn-outline-success").click();
 
   cy.get("#loginEmail").type("linda@stud.noroff.no");
   cy.get("#loginPassword").type("Test1234");
