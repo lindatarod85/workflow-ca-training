bf3444bb959016ab77f0d6ea826216ce042e575f
lindatarod85 <lindatarod@gmail.com>
2023-09-28 09:46:23 +0200

    x

---
+++ cypress/e2e/3-testing/logout.cy.js
@@ -0,0 +1,7 @@
+it("Logs out user", () => {
+  cy.visit("http://127.0.0.1:5500/?view=profile&name=linda");
+
+  cy.get(".btn-outline-warning").click();
+
+  cy.location("pathname").should("eq", "http://127.0.0.1:5500/index.html");
+});
