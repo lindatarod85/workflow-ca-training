7cbe4083c44e106581d4a87495d07322369d053e
lindatarod85 <lindatarod@gmail.com>
2023-09-28 10:02:02 +0200

    x

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -19,19 +19,8 @@ it("signup and login user", () => {
   );
 
   //logout
-  //cy.visit("http://127.0.0.1:5500/?view=profile&name=linda");
-
-  //cy.get(".btn-outline-warning").click();
-
-  //cy.location("pathname").should("eq", "http://127.0.0.1:5500/index.html");
-});
-
-/*
-it("Logs out user", () => {
-  cy.visit("http://127.0.0.1:5500/?view=profile&name=linda");
 
   cy.get(".btn-outline-warning").click();
 
   cy.location("pathname").should("eq", "http://127.0.0.1:5500/index.html");
 });
-*/
