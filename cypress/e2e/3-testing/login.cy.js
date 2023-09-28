5de32c93962614be867c5d7abc43cccc48f94ba5
lindatarod85 <lindatarod@gmail.com>
2023-09-28 09:42:47 +0200

    x

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -21,7 +21,7 @@ it("signup and login user", () => {
   //logout
   //cy.visit("http://127.0.0.1:5500/?view=profile&name=linda");
 
-  //cy.get(".btn-outline-warning").click();
+  cy.get(".btn-outline-warning").click();
 
-  //cy.location("pathname").should("eq", "http://127.0.0.1:5500/index.html");
+  cy.location("pathname").should("eq", "http://127.0.0.1:5500/index.html");
 });
